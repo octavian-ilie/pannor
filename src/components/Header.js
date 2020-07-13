a4231ace3cb499002768dec82b380a502c1c351c
octavian-ilie <octavian.ilie@icloud.com>
2020-07-13 09:22:49 +0200

    Replaced CSS with styled-components.

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,24 @@ import logo from '../img/pannor-logo-transparent_new.png';
 import { MyPannor } from './MyPannor';
 import { SearchIcon } from './SearchIcon';
 
+const HeaderContainer = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  transition: all 0.3s ease-in-out;
+  border-bottom: 1px solid #dbdbdb;
+`;
+
+const PrivBusWrapper = styled.div`
+  width: 1170px;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+`;
+
 const NavLink = styled(Link)`
   color: #222;
   text-decoration: none;
@@ -16,7 +34,7 @@ const NavLink = styled(Link)`
 
   :hover {
     color: #fa7268;
-  }  
+  }
 `;
 
 const HeaderIconLink = styled(Link)`
@@ -29,35 +47,73 @@ const HeaderIconLink = styled(Link)`
   }
 `;
 
+const HeaderWrapper = styled.div`
+  width: 1170px;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+  padding-bottom: 20px;
+`;
+
+const NavContainer = styled.div`
+  height: auto;
+  width: 40%;
+  display: flex;
+  align-items: center;
+`;
+
+const LogoContainer = styled.div`
+  height: auto;
+  width: 20%;
+  display: flex;
+  justify-content: center;
+`;
+
+const BrandLogo = styled.img`
+  height: 60px;
+  margin-top: -12px;
+  transition: all 0.3s ease;
+`;
+
+const IconsContainer = styled.div`
+  height: auto;
+  width: 40%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
 function Header() {
   return (
-    <div className="header-container">
-      <div className="header-privbus-wrapper">
+    <HeaderContainer>
+      <PrivBusWrapper>
         <PrivBusLink to="/">Private</PrivBusLink>
         <PrivBusLink to="/business">Business</PrivBusLink>
-      </div>
-      <div className="header-wrapper">
-        <div className="header--nav-container">
+      </PrivBusWrapper>
+      <HeaderWrapper>
+        <NavContainer>
           <NavLink to="/shop">Shop</NavLink>
           <NavLink to="/mobile">Mobile</NavLink>
           <NavLink to="/landline">Internet & TV</NavLink>
           <NavLink to="/support">Support</NavLink>
-        </div>
-        <div className="header--logo-container">
+        </NavContainer>
+        <LogoContainer>
           <Link to="/">
-            <img className="brand-logo" src={logo} alt="Pannor"/>
+            <BrandLogo src={logo} alt="Pannor"/>
           </Link>
-        </div>
-        <div className="header--my-container">
+        </LogoContainer>
+        <IconsContainer>
           <HeaderIconLink to="/search">
             <SearchIcon/>
           </HeaderIconLink>
           <HeaderIconLink to="/my">
             <MyPannor/>
           </HeaderIconLink>
-        </div>
-      </div>
-    </div>
+        </IconsContainer>
+      </HeaderWrapper>
+    </HeaderContainer>
   )
 }
 
