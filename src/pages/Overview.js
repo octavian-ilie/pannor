ade1eb23e40f211bfff250822547c590db6f208d
octavian-ilie <octavian.ilie@icloud.com>
2020-07-21 10:21:22 +0200

    Styling

--- src/pages/Overview.js
+++ src/pages/Overview.js
@@ -19,6 +19,13 @@ const HeaderContainer = styled.div`
   justify-content: space-between;
 `;
 
+const AppContainer = styled.div`
+  width: 1170px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
 const Title = styled.div`
   font-family: 'Octavian Regular', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   font-size: 2rem;
@@ -26,17 +33,29 @@ const Title = styled.div`
   padding: 4rem 0;
 `;
 
-const ChooseNumber = styled.select`
-  height: 60px;
-  width: 160px;
-  outline: none;
+const ChooseNumber = styled.div`
+  background-color: #fff;
   align-self: center;
   font-size: 1.1rem;
   padding: 1rem;
   border-radius: 3px;
-  border: 1px solid #dbdbdb;
-  color: #222;
+  color: #555;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);
+
+  &:hover {
+    color: #222;
+    box-shadow: 0 1px 2px rgba(0,0,0,0.02),
+                0 2px 4px rgba(0,0,0,0.02),
+                0 4px 8px rgba(0,0,0,0.02),
+                0 8px 16px rgba(0,0,0,0.02),
+                0 16px 32px rgba(0,0,0,0.02),
+                0 32px 64px rgba(0,0,0,0.02);
+  }
 `;
+
+
 
 function Overview(props) {
   document.title = defaultTitle + 'My overview';
@@ -51,20 +70,18 @@ function Overview(props) {
     persGreeting = 'Good afternoon, ';
   } else persGreeting = 'Good evening, ';
 
+  const userName = 'Octavian';
+  const currentNo = '0788123456'
+
   return (
     <Background>
       <HeaderContainer>
         <Title>
-          {persGreeting}userName
+          {persGreeting}{userName}
         </Title>
         <ChooseNumber>
-          <option>
-            0788123456
-          </option>
-          <option>
-            0788000111
-          </option>
-          </ChooseNumber>
+          {currentNo}
+        </ChooseNumber>
       </HeaderContainer>
     </Background>
   );
