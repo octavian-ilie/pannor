d2227e22ba355a8c03925b45af0671e9b95c7407
octavian-ilie <octavian.ilie@icloud.com>
2020-08-04 13:12:41 +0200

    Mobile menu closes on other links.

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,25 +28,27 @@ function Header() {
   const node = useRef();
   useOnClickOutside(node, () => setOpen(false));
 
+  const closeMobileMenu = () => setOpen(false);
+
   return (
     <HeaderContainer ref={node}>
       <PrivBusWrapper>
-        <PrivBusLink exact to="/">Private</PrivBusLink>
-        <PrivBusLink exact to="/business">Business</PrivBusLink>
+        <PrivBusLink exact to="/" onClick={closeMobileMenu}>Private</PrivBusLink>
+        <PrivBusLink exact to="/business" onClick={closeMobileMenu}>Business</PrivBusLink>
       </PrivBusWrapper>
       <HeaderWrapper>
         <NavContainer>
-          <NavLink to="/shop">Shop</NavLink>
+          <NavLink to="/shop" onClick={closeMobileMenu}>Shop</NavLink>
           <TriggerLink open={open} setOpen={setOpen} onClick={() => setOpen(!open)}>Mobile</TriggerLink>
-          <NavLink to="/landline">Internet & TV</NavLink>
-          <NavLink to="/support">Support</NavLink>
+          <NavLink to="/landline" onClick={closeMobileMenu}>Internet & TV</NavLink>
+          <NavLink to="/support" onClick={closeMobileMenu}>Support</NavLink>
           <div id="menu-toggle">
             <input className="menu-btn" type="checkbox" id="menu-btn" />
             <label className="menu-icon" htmlFor="menu-btn"><span className="navicon"></span></label>
           </div>
         </NavContainer>
         <LogoContainer>
-          <Link to="/">
+          <Link to="/" onClick={closeMobileMenu}>
             <BrandLogo src={logo} alt="Pannor"/>
           </Link>
         </LogoContainer>
@@ -54,12 +56,12 @@ function Header() {
           <HeaderIconLink to="/search">
             <SearchIcon/>
           </HeaderIconLink>
-          <HeaderIconLink to="/signin">
+          <HeaderIconLink to="/signin" onClick={closeMobileMenu}>
             <MyPannor/>
           </HeaderIconLink>
         </IconsContainer>
       </HeaderWrapper>
-      <HeaderMobileMenu open={open} setOpen={setOpen}/>
+      <HeaderMobileMenu open={open} setOpen={setOpen} />
     </HeaderContainer>
   )
 }
